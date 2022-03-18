14c3ce9c4e0b5d4bd91d0210ee45ee4da22134e5
lipeRefosco <luisfelipe.refosco@hotmail.com>
2022-03-18 10:13:43 -0300

    Interface dos serviços

--- src/Classes/Domain/Interfaces/ServicesInterface.php
+++ src/Classes/Domain/Interfaces/ServicesInterface.php
@@ -5,4 +5,10 @@ namespace App\Domain\Interfaces;
 interface ServicesInterface
 {
     public static function get();
+
+    public static function post();
+
+    public static function put();
+    
+    public static function delete();
 }
